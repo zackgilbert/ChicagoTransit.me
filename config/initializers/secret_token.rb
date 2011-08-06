286212c97a75d806475897ff5507ee7a7f7bd355
Zack Gilbert <zackgilbert@gmail.com>
2011-08-06 00:17:34 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChicagoTransit::Application.config.secret_token = '4c68b36b4f604cfa0719a341fe67f3113c47aca612f0cfea05da167839b46e8ffaf399f4d4fa2ae234ea86c04cafd69272694e8bbb87f71d4106e1e562c242d1'
