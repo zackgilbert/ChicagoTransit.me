7c4e769854ec7575ee45e6ef4807c4cf01717302
Zack Gilbert <zackgilbert@gmail.com>
2011-08-28 23:36:03 -0500

    Adding geo.js to handle cross browser/platform geo stuff.

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,23 +1,22 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
-	function geoError(err) {
+	function geo_error(err) {
 		alert("Error: " + err.message);
 		//console.log('failed');
 	  //console.log(err);
 		//console.log(arguments);
 	}
 
-	function relocateSuccess(position) {
+	function relocate_success(position) {
 		//console.log('success');
 		window.location.href = '/?lat=' + position.coords.latitude + '&lng=' + position.coords.longitude + '&accuracy=' + position.coords.accuracy;
 	}
 
 	function relocate() {
 		//console.log('hi');
-		if (navigator.geolocation) {
-			//console.log('searching...')
-	  	navigator.geolocation.getCurrentPosition(relocateSuccess, geoError);
+		if (geo_position_js.init()) {
+		  geo_position_js.getCurrentPosition(relocate_success, geo_error);
 		} else {
 	  	error('Your browser does not support geolocation.');
 		}
